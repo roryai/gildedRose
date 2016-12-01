e38e954a0b7f93abf757a4e6bd25f808405cc52a
Rory Aitchison <rory.aitchison@gmail.com>
2016-12-01 11:10:01 +0000

    adds 2 tests for standard items

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -1,9 +1,22 @@
 describe("Gilded Rose", function() {
 
-  it("should foo", function() {
-    items = [ new Item("foo", 0, 0) ];
-    update_quality();
-    expect(items[0].name).toEqual("fixme");
+  describe('sellIn', function() {
+
+    it('Reduces sellIn- standard item', function(){
+      items = [ new Item('Elixir of the Mongoose', 5, 7) ];
+      update_quality();
+      expect(items[0].sell_in).toEqual(4);
+    });
+
   });
 
+  describe('sellIn', function() {
+
+    it('Increases quality- standard item', function(){
+      items = [ new Item('Elixir of the Mongoose', 5, 7) ];
+      update_quality();
+      expect(items[0].quality).toEqual(6);
+    });
+
+  });
 });
