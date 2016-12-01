354d3da3c5bd0fcd699c5b01da39156fdbbfb3c1
Rory Aitchison <rory.aitchison@gmail.com>
2016-12-01 15:10:07 +0000

    adds constants

--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -4,6 +4,18 @@ function Item(name, sell_in, quality) {
   this.quality = quality;
 }
 
+var STANDARD_QUALITY_DECREASE = 1;
+var STANDARD_PAST_SELL_IT_MOD = 2;
+var TICKETS_11_PLUS_DAYS_MOD = 1;
+var TICKETS_10_TO_6_DAYS_MOD = 2;
+var TICKETS_5_TO_1_DAYS_MOD = 3;
+var TICKETS_0_DAYS_QUALITY = 0;
+var QUALITY_UPPER_LIMIT = 50;
+var QUALITY_LOWER_LIMIT = 0;
+var CONJURED_QUALITY_MOD = 2;
+var BRIE_QUALITY_MOD = 1;
+
+
 var items = []
 
 function qualityDecrease(item, modifier) {
@@ -15,7 +27,8 @@ function update_quality() {
     if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
       if (items[i].quality > 0) {
         if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-          items[i].quality = items[i].quality - 1
+          // NEEDS EXCEPTION HERE FOR CONJRUED ITEMS
+          qualityDecrease(items[i], STANDARD_QUALITY_DECREASE)
         }
       }
     } else {
