d531b53a324b5520704b75317d799b3da4f29acb
Rory Aitchison <rory.aitchison@gmail.com>
2016-12-01 15:40:36 +0000

    some refactoring

--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -4,22 +4,34 @@ function Item(name, sell_in, quality) {
   this.quality = quality;
 }
 
-var STANDARD_QUALITY_DECREASE = 1;
-var STANDARD_PAST_SELL_IT_MOD = 2;
-var TICKETS_11_PLUS_DAYS_MOD = 1;
-var TICKETS_10_TO_6_DAYS_MOD = 2;
-var TICKETS_5_TO_1_DAYS_MOD = 3;
-var TICKETS_0_DAYS_QUALITY = 0;
-var QUALITY_UPPER_LIMIT = 50;
-var QUALITY_LOWER_LIMIT = 0;
-var CONJURED_QUALITY_MOD = 2;
-var BRIE_QUALITY_MOD = 1;
+var STANDARD_QUALITY_MOD      = -1;
+var STANDARD_PAST_SELL_IT_MOD = -2;
+var TICKETS_11_PLUS_DAYS_MOD  = 1;
+var TICKETS_10_TO_6_DAYS_MOD  = 2;
+var TICKETS_5_TO_1_DAYS_MOD   = 3;
+var CONJURED_QUALITY_MOD      = -2;
+var BRIE_QUALITY_MOD          = 1;
 
+var TICKETS_0_DAYS_QUALITY    = 0;
+var QUALITY_UPPER_LIMIT       = 50;
+var QUALITY_LOWER_LIMIT       = 0;
 
 var items = []
 
-function qualityDecrease(item, modifier) {
-  item.quality -= modifier;
+function qualityChange(item, modifier) {
+  item.quality += modifier;
+};
+
+function sellInChange(item) {
+  item.sell_in -= 1;
+};
+
+function standardQualityHandler(item) {
+  qualityChange(item, STANDARD_QUALITY_MOD);
+};
+
+function brieQualityHandler(item) {
+  qualityChange(item, BRIE_QUALITY_MOD);
 };
 
 function update_quality() {
@@ -28,7 +40,7 @@ function update_quality() {
       if (items[i].quality > 0) {
         if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
           // NEEDS EXCEPTION HERE FOR CONJRUED ITEMS
-          qualityDecrease(items[i], STANDARD_QUALITY_DECREASE)
+          standardQualityHandler(items[i]);
         }
       }
     } else {
@@ -49,7 +61,7 @@ function update_quality() {
       }
     }
     if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-      items[i].sell_in = items[i].sell_in - 1;
+      sellInChange(items[i]);
     }
     if (items[i].sell_in < 0) {
       if (items[i].name != 'Aged Brie') {
@@ -64,7 +76,7 @@ function update_quality() {
         }
       } else {
         if (items[i].quality < 50) {
-          items[i].quality = items[i].quality + 1
+          brieQualityHandler(items[i])
         }
       }
     }
